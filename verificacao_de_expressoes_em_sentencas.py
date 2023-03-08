c315b412b75f164e1b97e2d01a75b3faa1d1179a
ThiAABrown <antunesabrown@gmail.com>
2023-03-08 10:59:42 -0300

    realizado parte 2 e 3/6 do desafio 3

--- verificacao_de_expressoes_em_sentencas.py
+++ verificacao_de_expressoes_em_sentencas.py
@@ -1,14 +1,20 @@
-# 1 -ler o arquivo de expressões e guardar o resultado em uma lista
+import json
 
+#__import__('ipdb').set_trace()
+
+# 1 -ler o arquivo de expressões e guardar o resultado em uma lista
 with open('expressoes.txt', 'r') as expressoes_arquivo:
         expressoes = expressoes_arquivo.readlines()
-__import__('ipdb').set_trace()
 
 # 2- ler o arquivo de entrada (textos.json) e guardar o conteúdo em uma variável usando a funçâo "json.loads"
+with open('textos.json') as entrada_textos:
+    textos = json.load(entrada_textos)
 
-# 3 - criar uma lista para armazenar o output 
+# 3 - criar uma lista para armazenar a saida
+saida = []
 
-# 4 - para cada sentença (.?!) do arquivo de entrada, iterar na lista de expressôes e verificar se ela comea com a expressâo. 
+# 4 - para cada sentença (.?!) do arquivo de entrada, iterar na lista de expressôes e verificar se ela começa com a expressâo. 
+
 
 # 5 - Criar um dict na lista que armazena o output com a sentenca e se ela possui ou nao a expressao.
 
